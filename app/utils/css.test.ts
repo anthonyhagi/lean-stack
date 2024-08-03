443bc90d75e6cbb7620eeee2ec39746a68421817
Anthony Hagidimitriou <info@anthonyhagi.com.au>
2024-08-03 19:24:17 +0930

    feat: add vitest testing framework for unit tests

---
+++ app/utils/css.test.ts
@@ -0,0 +1,9 @@
+import { describe, it } from "vitest";
+
+import { cn } from "./css";
+
+describe("utils/css", () => {
+  it("correctly merges css", ({ expect }) => {
+    expect(cn("one", "two")).toBe("one two");
+  });
+});
---
+++ vitest.config.ts
@@ -0,0 +1,21 @@
+/// <reference types="vitest" />
+
+import { resolve } from "path";
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  test: {
+    environment: "jsdom",
+    globals: true,
+    includeSource: ["app/**/*.{ts,tsx}"],
+    exclude: ["node_modules", "e2e"],
+    env: {},
+  },
+  resolve: {
+    alias: {
+      "~": resolve(__dirname, "app"),
+    },
+  },
+  plugins: [react()],
+});
