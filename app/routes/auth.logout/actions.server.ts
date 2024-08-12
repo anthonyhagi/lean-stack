3c1e798119f09be4fa2e2e46d8f70fe3d89a6886
Anthony Hagidimitriou <info@anthonyhagi.com.au>
2024-08-12 23:01:32 +0930

    chore(docs): add description for logout function

--- app/routes/auth.logout/actions.server.ts
+++ app/routes/auth.logout/actions.server.ts
@@ -3,6 +3,15 @@ import { redirect } from '@remix-run/node';
 import { AUTH_SESSION_KEY, authSessionStorage } from '~/services/auth';
 import { deleteSession } from '~/services/auth';
 
+/**
+ * Handle logging a user out by ensuring that their session is destroyed
+ * both on the server and on the client (the cookie).
+ *
+ * @param request the request object from the loader.
+ *
+ * @throws a redirect to the homepage to ensure that the cookie is
+ * destroyed properly.
+ */
 export async function logout(request: Request) {
   const cookie = request.headers.get('Cookie');
   const authSession = await authSessionStorage.getSession(cookie);
