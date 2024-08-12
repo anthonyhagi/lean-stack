20b3d32c1330aacf60b090f2efbc63fb213d2c5e
Anthony Hagidimitriou <info@anthonyhagi.com.au>
2024-08-12 23:19:33 +0930

    feat: add google login button if the credentials are available

--- app/routes/auth.login/route.tsx
+++ app/routes/auth.login/route.tsx
@@ -1,5 +1,5 @@
 import { LoaderFunctionArgs } from '@remix-run/node';
-import { Form } from '@remix-run/react';
+import { Form, Link, useLoaderData } from '@remix-run/react';
 import { Button } from '~/components/ui/button';
 import {
   Card,
@@ -17,14 +17,28 @@ import { parseWithZod } from '@conform-to/zod';
 import { schema } from './validation';
 import { Field, FieldError, InputConform } from '~/components/form';
 import { CheckboxConform } from '~/components/form/checkbox';
+import { env } from '~/services/app';
+import { GoogleLogo } from '~/components/social/google';
+import { Separator } from '~/components/ui/separator';
 
 export async function loader({ request }: LoaderFunctionArgs) {
   await requireNotLoggedIn(request);
 
-  return null;
+  const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = env;
+
+  return {
+    features: {
+      // This is to ensure that if the environment variables aren't
+      // available, we shouldn't allow nor even show the google
+      // auth feature.
+      googleAuthEnabled: !!GOOGLE_CLIENT_ID && !!GOOGLE_CLIENT_SECRET,
+    },
+  };
 }
 
 export default function Page() {
+  const data = useLoaderData<typeof loader>();
+
   const [form, fields] = useForm({
     shouldValidate: 'onBlur',
     shouldRevalidate: 'onBlur',
@@ -105,10 +119,40 @@ export default function Page() {
               <FieldError>{form.errors}</FieldError>
             </div>
           ) : null}
+
+          <Button className="w-full">Sign in</Button>
         </CardContent>
 
-        <CardFooter>
-          <Button className="w-full">Sign in</Button>
+        <CardFooter className="flex-col">
+          <div
+            className={cn(
+              'w-full',
+              data.features.googleAuthEnabled ? 'mb-6' : 'mb-4'
+            )}>
+            <Separator />
+          </div>
+
+          {data.features.googleAuthEnabled ? (
+            <Form method="POST" action="/auth/google" className="w-full">
+              <Button variant="outline" className="w-full">
+                <span className="mr-2 inline-flex h-3.5 w-3.5">
+                  <GoogleLogo />
+                </span>{' '}
+                Login with Google
+              </Button>
+            </Form>
+          ) : null}
+
+          <div
+            className={cn(
+              'text-center text-sm',
+              data.features.googleAuthEnabled ? 'mt-4' : ''
+            )}>
+            Don&apos;t have an account?{' '}
+            <Link to="/auth/register" className="underline">
+              Sign up
+            </Link>
+          </div>
         </CardFooter>
       </Card>
     </Form>
