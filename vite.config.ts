2d03f1ea36237a55ecc146796c895ff9cce679ea
Anthony Hagidimitriou <info@anthonyhagi.com.au>
2025-06-10 22:37:44 +0930

    fix: use proper vite file

--- vite.config.ts
+++ vite.config.ts
@@ -2,31 +2,11 @@ import { reactRouter } from '@react-router/dev/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-declare module '@remix-run/node' {
-  // or cloudflare, deno, etc.
-  interface Future {
-    v3_singleFetch: true;
-  }
-}
-
 export default defineConfig({
   optimizeDeps: {
     // For an explanation as to why this was added, refer to:
     // https://github.com/smithy-lang/smithy-typescript/issues/1437#issuecomment-2508474767
     exclude: ['@smithy/shared-ini-file-loader'],
   },
-  plugins: [
-    reactRouter({
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-        unstable_optimizeDeps: true,
-        v3_lazyRouteDiscovery: true,
-        v3_routeConfig: true,
-        v3_singleFetch: true,
-      },
-    }),
-    tsconfigPaths(),
-  ],
+  plugins: [reactRouter(), tsconfigPaths()],
 });
